7de73f4e07f7185002b5f56539ab3157c8b511d7
rmayhem1 <robmahin.dev@gmail.com>
2025-04-16 13:12:29 -0700

    Walljumping & Mini Course

--- MovementGameDevProject/Assets/Scripts/Player/WallRun.cs
+++ MovementGameDevProject/Assets/Scripts/Player/WallRun.cs
@@ -8,10 +8,13 @@ public class Wallrun : MonoBehaviour
     public LayerMask whatIsWall;
     public LayerMask whatIsGround;
     public float wallRunForce;
+    public float wallJumpUpForce;
+    public float wallJumpSideForce;
     public float maxWallRunTime;
     private float wallRunTimer;
 
     [Header("Input")]
+    public KeyCode jumpKey = KeyCode.Space;
     private float horizontalInput;
     private float verticalInput;
 
@@ -22,6 +25,11 @@ public class Wallrun : MonoBehaviour
     private RaycastHit rightWallHit;
     private bool wallLeft;
     private bool wallRight;
+
+    [Header("Exiting")]
+    private bool exitingWall;
+    public float exitWallTime;
+    private float exitWallTimer;
 
     [Header("References")]
     public Transform orientation;
@@ -64,10 +72,26 @@ public class Wallrun : MonoBehaviour
         verticalInput = Input.GetAxisRaw("Vertical");
 
         // State 1 : Wallrunning
-        if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround())
+        if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround() && !exitingWall)
         {
             if (!pm.wallrunning)
                 StartWallRun();
+
+            // wall jump
+            if (Input.GetKeyDown(jumpKey)) 
+                WallJump();
+        }
+        // State 2 - Exiting
+        else if (exitingWall)
+        {
+            if (pm.wallrunning)
+                StopWallRun();
+
+            if (exitWallTimer > 0)
+                exitWallTimer -= Time.deltaTime;
+
+            if (exitWallTimer < 0)
+                exitingWall = false;
         }
         // State 3 - None
         else
@@ -105,5 +129,20 @@ public class Wallrun : MonoBehaviour
     private void StopWallRun()
     {
         pm.wallrunning = false;
+    }
+
+    private void WallJump()
+    {
+        // enter exiting wall state
+        exitingWall = true;
+        exitWallTimer = exitWallTime;
+
+        Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+
+        Vector3 forceToApply = transform.up * wallJumpUpForce + wallNormal * wallJumpSideForce;
+
+        // add force & reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(forceToApply, ForceMode.Impulse);
     }
 }
