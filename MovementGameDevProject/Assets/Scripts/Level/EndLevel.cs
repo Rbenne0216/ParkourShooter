8d392c7ec96819fdac11bb0357a21834536a9208
rmayhem1 <robmahin.dev@gmail.com>
2025-04-30 21:52:25 -0700

    Testing with EndLevel.cs & Tutorial Design

---
+++ MovementGameDevProject/Assets/Scripts/Level/EndLevel.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    /*
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("SampleScene");
+            Debug.Log("Scene Loaded, John hates Raf");
+        }
+    }
+}
